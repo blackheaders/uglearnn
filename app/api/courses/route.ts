4b57ccb06c33ae9f33657007b25b7f09abca5b9c
Aditya Bansal <adityabansalsafe@gmail.com>
2025-02-05 03:15:42 +0530

    Add : no cache Control

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -5,7 +5,11 @@ import { getCourses } from "@/db/courses";
 export async function GET() {
   try {
     const courses = await getCourses();
-    return NextResponse.json(courses);
+    return NextResponse.json(courses, {
+      headers: {
+        "Cache-Control": "no-store", // Disable all caching
+      },
+    });
   } catch (error) {
     console.log("[COURSES]", error);
     return new NextResponse("Internal Error", { status: 500 });
