6c4c8b7ead8320b73c2db6b9c12397509cc8f099
Aditya Bansal <adityabansalsafe@gmail.com>
2025-02-26 19:50:58 +0530

    Build Fix

--- app/buy-course/page.tsx
+++ app/buy-course/page.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Course } from "@/types/types";
 import { useSession } from "next-auth/react";
-import { StripeCheckout } from "@/components/stripe-checkout";
+// import { StripeCheckout } from "@/components/stripe-checkout";
 import Image from "next/image";
 import { FaUniversity, FaBookOpen, FaCalendarAlt, FaShoppingCart, FaSpinner } from 'react-icons/fa';
 
